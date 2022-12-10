0a73c88f132f2c3625c365e8da59ce1165b3a763
Zakum <mirveo@gmail.com>
2022-12-10 08:35:38 -0800

    /class refactor

--- Commands/Public/class.js
+++ Commands/Public/class.js
@@ -1,24 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getClassInfo } = require('../../Functions/classInfo');
 const text = require("../../Text/en/classes.json");
-const cmdText = require("../../Text/en/commands.json");
+const commandTexts = require("../../Text/en/commands.json");
+const { name, description, options } = commandTexts.class
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName(cmdText.class.name)
-        .setDescription(cmdText.class.description)
+        .setName(name)
+        .setDescription(description)
         .addStringOption(option =>
             option
-                .setName(cmdText.class.options[0].name)
-                .setDescription(cmdText.class.options[0].description)
+                .setName(options[0].name)
+                .setDescription(options[0].description)
                 .addChoices(
-                    { name: text.classes["aegis-fighter"].name, value: text.classes["aegis-fighter"].uri },
-                    { name: text.classes["blast-archer"].name, value: text.classes["blast-archer"].uri },
-                    { name: text.classes["spell-caster"].name, value: text.classes["spell-caster"].uri },
-                    { name: text.classes["twin-striker"].name, value: text.classes["twin-striker"].uri }
+                    ...Object.keys(text.classes).map(choice => ({ 
+                        name: text.classes[choice].name, 
+                        value: text.classes[choice].uri 
+                    }))
                 )),
     execute(interaction) {
-        const selection = interaction.options.getString(cmdText.class.options[0].name)
+        const selection = interaction.options.getString(options[0].name)
         return interaction.reply(getClassInfo(selection))
     },
 };
