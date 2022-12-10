e3227d8ad5399ee408641d3f2322f53d0db12eb8
Zakum <mirveo@gmail.com>
2022-12-10 08:04:35 -0800

    update /class className

--- Commands/Public/class.js
+++ Commands/Public/class.js
@@ -10,10 +10,10 @@ module.exports = {
                 .setName('class')
                 .setDescription('Information about a specific class.')
                 .addChoices(
-                    { name: 'Aegis Fighter', value: 'aegisfighter' },
-                    { name: 'Blast Archer', value: 'blastarcher' },
-                    { name: 'Spell Caster', value: 'spellcaster' },
-                    { name: 'Twin Striker', value: 'twinstriker' },
+                    { name: 'Aegis Fighter', value: 'aegis-fighter' },
+                    { name: 'Blast Archer', value: 'blast-archer' },
+                    { name: 'Spell Caster', value: 'spell-caster' },
+                    { name: 'Twin Striker', value: 'twin-striker' },
                 )),
     execute(interaction) {
         const selection = interaction.options.getString('class')
